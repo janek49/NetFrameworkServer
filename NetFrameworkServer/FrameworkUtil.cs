9a1cee35c446116ca83f021ac1e911813098e302
Jan Bartula <janbartula@op.pl>
2021-07-16 23:42:36 +0200

    Initial commit

---
+++ NetFrameworkServer/FrameworkUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetFrameworkServer
+{
+    public class FrameworkUtil
+    {
+        public static T[] ArrayRange<T>(T[] array, int start, int end)
+        {
+            T[] tmp = new T[end - start];
+            Array.Copy(array, start, tmp, 0, end);
+            return tmp;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NetFrameworkServer;
+
+namespace NetFrameworkServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           TestHttpServer t = new TestHttpServer(IPAddress.Any, 80);
+           t.Start();
+
+           while (true) ;
+        }
+    }
+
+    class TestHttpSession : HttpSession
+    {
+        public TestHttpSession(HttpServer server) : base(server)
+        {
+        }
+        protected override void OnReceivedRequest(HttpRequest request)
+        {
+            SendResponseAsync(Response.MakeGetResponse("<h1>Hello World!</h1>", "text/html"));
+        }
+    }
+
+    class TestHttpServer : HttpServer
+    {
+        public TestHttpServer(IPAddress address, int port) : base(address, port)
+        {
+        }
+
+        public TestHttpServer(string address, int port) : base(address, port)
+        {
+        }
+
+        public TestHttpServer(IPEndPoint endpoint) : base(endpoint)
+        {
+        }
+
+        protected override TcpSession CreateSession() { return new TestHttpSession(this); }
+    }
+}
