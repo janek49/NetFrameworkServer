51208fec60f42019ee425510314b6714c5327f72
Jan Bartula <janbartula@op.pl>
2021-07-17 12:40:34 +0200

    Bugfix

--- NetFrameworkServer/FrameworkUtil.cs
+++ NetFrameworkServer/FrameworkUtil.cs
@@ -7,7 +7,7 @@ namespace NetFrameworkServer
         public static T[] ArrayRange<T>(T[] array, int start, int end)
         {
             T[] tmp = new T[end - start];
-            Array.Copy(array, start, tmp, 0, tmp.length);
+            Array.Copy(array, start, tmp, 0, tmp.Length);
             return tmp;
         }
     }
